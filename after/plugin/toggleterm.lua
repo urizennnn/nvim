87ed5d8b702626fe20160e8a1dad385f26f7d05a
urizennnn <igamerryt@gmail.com>
2024-03-09 18:58:22 +0100

    changed keybind to open terminal

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,11 +1,10 @@
 require ('toggleterm').setup{
 size = 13,
-open_mapping=[[<Esc>]],
+open_mapping=[[<C-n>]],
 shade_filetypes = {},
 shade_terminals = true,
 shading_factor = 1,
 start_in_insert = true,
 persist_size = true,
 direction = 'horizontal'
-
 }
