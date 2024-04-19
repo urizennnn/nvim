0011c9b459f1393ebb6e6c379b6f33376b722cd1
urizennnn <igamerryt@gmail.com>
2024-04-19 18:18:07 +0100

    installed laxzgit

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require("telescope.builtin")
+require("telescope").load_extension("lazygit")
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
