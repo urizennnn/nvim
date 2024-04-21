b0551e4e05a62853ace7d7d7bb548f102b2bbb02
urizennnn <igamerryt@gmail.com>
2024-04-21 22:24:55 +0100

    made neovim transaprent
    only if your terminal has a background

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -3,10 +3,58 @@ require("nvim-tree").setup({
 		sorter = "case_sensitive",
 	},
 	view = {
+		centralize_selection = false,
+		cursorline = true,
+		debounce_delay = 15,
+		side = "right",
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
 		width = 30,
+		float = {
+			enable = true,
+			quit_on_focus_loss = true,
+			open_win_config = {
+				relative = "editor",
+				border = "rounded",
+				width = 30,
+				height = 30,
+				row = 1,
+				col = 1,
+			},
+		},
 	},
 	renderer = {
 		group_empty = true,
+		icons = {
+			web_devicons = {
+				file = {
+					enable = true,
+					color = true,
+				},
+				folder = {
+					enable = true,
+					color = true,
+				},
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				bookmark = "󰆤",
+				modified = "●",
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+			},
+		},
 	},
 	filters = {
 		dotfiles = true,
--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 require("urizen")
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 -- optionally enable 24-bit colour
-vim.opt.termguicolors = true
--- Set <space> as the leader key
+vim.opt.termguicolors = true -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
@@ -720,15 +719,20 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			-- any ot
+			-- her, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
+		end,
+		config = function()
+			require("after.plugin.folke")
 		end,
 	},
 
