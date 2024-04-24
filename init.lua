3d1a2cf8840174736ff2f186850ab5a2e0a9e621
urizennnnnn <igamerryt@gmail.com>
2024-04-24 23:48:20 +0100

    fixed cursor line

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("urizen")
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
@@ -22,7 +23,6 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 -- vim.opt.relativenumber = true
 
-vim.opt.cursorline = false
 -- Disable highlighting
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = true
@@ -63,9 +63,6 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
