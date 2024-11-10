2f609c9a322c7b6ddb08bfaf474a596a7a7b1d21
urizennnn <igamerryt@gmail.com>
2024-11-10 18:50:34 +0100

    refactor:refactor the snacks config

--- after/plugin/snacks.lua
+++ after/plugin/snacks.lua
@@ -1,157 +1,118 @@
+vim.print("Snacks loaded")
 local opts = {
-	{
-		priority = 1000,
-		lazy = false,
-		---@type snacks.Config
-		opts = {
-			bigfile = { enabled = true },
-			notifier = {
-				enabled = true,
-				timeout = 3000,
-			},
-			quickfile = { enabled = true },
-			statuscolumn = { enabled = true },
-			words = { enabled = true },
-			styles = {
-				notification = {
-					wo = { wrap = true }, -- Wrap notifications
-				},
-			},
+	bigfile = {
+		enabled = true,
+		setup = function(ctx)
+			vim.b.minianimate_disable = true
+			vim.schedule(function()
+				vim.bo[ctx.buf].syntax = ctx.ft
+			end)
+		end,
+	},
+	notifier = {
+		timeout = 3000, -- default timeout in ms
+		width = { min = 40, max = 0.4 },
+		height = { min = 1, max = 0.6 },
+		-- editor margin to keep free. tabline and statusline are taken into account automatically
+		margin = { top = 0, right = 1, bottom = 0 },
+		padding = true, -- add 1 cell of left/right padding to the notification window
+		sort = { "level", "added" }, -- sort by level and time
+		icons = {
+			error = " ",
+			warn = " ",
+			info = " ",
+			debug = " ",
+			trace = " ",
 		},
-		keys = {
-			{
-				"<leader>un",
-				function()
-					Snacks.notifier.hide()
-				end,
-				desc = "Dismiss All Notifications",
-			},
-			{
-				"<leader>bd",
-				function()
-					Snacks.bufdelete()
-				end,
-				desc = "Delete Buffer",
-			},
-			{
-				"<leader>gg",
-				function()
-					Snacks.lazygit()
-				end,
-				desc = "Lazygit",
-			},
-			{
-				"<leader>gb",
-				function()
-					Snacks.git.blame_line()
-				end,
-				desc = "Git Blame Line",
-			},
-			{
-				"<leader>gB",
-				function()
-					Snacks.gitbrowse()
-				end,
-				desc = "Git Browse",
-			},
-			{
-				"<leader>gf",
-				function()
-					Snacks.lazygit.log_file()
-				end,
-				desc = "Lazygit Current File History",
-			},
-			{
-				"<leader>gl",
-				function()
-					Snacks.lazygit.log()
-				end,
-				desc = "Lazygit Log (cwd)",
-			},
-			{
-				"<leader>cR",
-				function()
-					Snacks.rename()
-				end,
-				desc = "Rename File",
-			},
-			{
-				"<c-/>",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "Toggle Terminal",
-			},
-			{
-				"<c-_>",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "which_key_ignore",
-			},
-			{
-				"]]",
-				function()
-					Snacks.words.jump(vim.v.count1)
-				end,
-				desc = "Next Reference",
-				mode = { "n", "t" },
-			},
-			{
-				"[[",
-				function()
-					Snacks.words.jump(-vim.v.count1)
-				end,
-				desc = "Prev Reference",
-				mode = { "n", "t" },
-			},
-			{
-				"<leader>N",
-				desc = "Neovim News",
-				function()
-					Snacks.win({
-						file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-						width = 0.6,
-						height = 0.6,
-						wo = {
-							spell = false,
-							wrap = false,
-							signcolumn = "yes",
-							statuscolumn = " ",
-							conceallevel = 3,
-						},
-					})
-				end,
-			},
+		---@type snacks.notifier.style
+		style = "compact",
+		top_down = true, -- place notifications from top to bottom
+		date_format = "%R", -- time format for notifications
+		refresh = 50, -- refresh at most every 50ms
+	},
+	quickfile = { enabled = true },
+	statuscolumn = { enabled = true },
+	words = { enabled = true },
+	styles = {
+		notification = {
+			wo = { wrap = true }, -- Wrap notifications
 		},
-		init = function()
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "VeryLazy",
-				callback = function()
-					-- Setup some globals for debugging (lazy-loaded)
-					_G.dd = function(...)
-						Snacks.debug.inspect(...)
-					end
-					_G.bt = function()
-						Snacks.debug.backtrace()
-					end
-					vim.print = _G.dd -- Override print to use snacks for `:=` command
+	},
+}
 
-					-- Create some toggle mappings
-					Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-					Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-					Snacks.toggle.diagnostics():map("<leader>ud")
-					Snacks.toggle.line_number():map("<leader>ul")
-					Snacks.toggle
-						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-						:map("<leader>uc")
-					Snacks.toggle.treesitter():map("<leader>uT")
-					Snacks.toggle
-						.option("background", { off = "light", on = "dark", name = "Dark Background" })
-						:map("<leader>ub")
-					Snacks.toggle.inlay_hints():map("<leader>uh")
-				end,
+opts.keys = {
+	{
+		"<leader>un",
+		function()
+			Snacks.notifier.hide()
+		end,
+		desc = "Dismiss All Notifications",
+	},
+	{
+		"<leader>bd",
+		function()
+			Snacks.bufdelete()
+		end,
+		desc = "Delete Buffer",
+	},
+	{
+		"<leader>gb",
+		desc = "Git Blame Line",
+	},
+	{
+		"<leader>gB",
+		function()
+			Snacks.gitbrowse()
+		end,
+		desc = "Git Browse",
+	},
+
+	{
+		"<leader>gl",
+		function()
+			Snacks.lazygit.log()
+		end,
+		desc = "Lazygit Log (cwd)",
+	},
+	{
+		"<leader>R",
+		function()
+			Snacks.rename()
+		end,
+		desc = "Rename File",
+	},
+
+	{
+		"]]",
+		function()
+			Snacks.words.jump(vim.v.count1)
+		end,
+		desc = "Next Reference",
+		mode = { "n", "t" },
+	},
+	{
+		"[[",
+		function()
+			Snacks.words.jump(-vim.v.count1)
+		end,
+		desc = "Prev Reference",
+		mode = { "n", "t" },
+	},
+	{
+		"<leader>N",
+		desc = "Neovim News",
+		function()
+			Snacks.win({
+				file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+				width = 0.6,
+				height = 0.6,
+				wo = {
+					spell = false,
+					wrap = false,
+					signcolumn = "yes",
+					statuscolumn = " ",
+					conceallevel = 3,
+				},
 			})
 		end,
 	},
